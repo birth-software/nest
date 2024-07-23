556f603487a3a0a2b2c8084ae9debb8b7ad1a29d
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-07-23 21:29:38 +0200

    Restructure test system

--- debug.sh
+++ debug.sh
@@ -2,15 +2,21 @@
 
 set -e
 
+path=$1
+echo $path
+
 source ./compile.sh
 build_dir="build"
 exe_name="nest"
 exe_path=$build_dir/$exe_name
+debug_flags="-g"
+optimization_flags=""
+bootstrap_args="$path"
 
-compile $build_dir $exe_name "-g" "";
+compile $build_dir $exe_name $debug_flags $optimization_flags
 
 case "$OSTYPE" in
-    darwin*)  lldb $exe_path ;; 
-    linux*)   gf2 -ex r $exe_path ;;
+    darwin*)  lldb -- $exe_path $bootstrap_args;; 
+    linux*)   gf2 -ex r --args $exe_path $bootstrap_args;;
     *)        echo "unknown: $OSTYPE" ;;
 esac
