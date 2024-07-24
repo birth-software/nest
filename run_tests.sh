5174819a5b3c2132af4ca6dece8aa4b0f36a3771
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-07-24 11:00:17 +0200

    Compile in different optimization modes

--- run_tests.sh
+++ run_tests.sh
@@ -2,25 +2,47 @@
 
 set -e
 source ./compile.sh
+all=$1
 build_dir="build"
 exe_name="nest"
 exe_path=$build_dir/$exe_name
 debug_flags="-g"
 no_optimization_flags=""
-optimization_flags="-O3 -march=native"
 test_names="first"
 
-compile $build_dir $exe_name $debug_flags $no_optimization_flags;
+if [ "$all" == "1" ]
+then
+    optimization_modes=("" "-O1" "-O2 march=native", "-Os march=native" "-Oz march=native" "-O3 march=native")
+else
+    optimization_modes=("")
+fi
 
-printf "\n======================\n"
-printf "TESTS"
-printf "\n======================\n\n"
-for test_name in "${test_names[@]}"
+for optimization_mode in "${optimization_modes[@]}"
 do
-    echo "$test_name..."
-    build/nest "tests/$test_name.nat"
-    echo "$test_name [COMPILATION] [OK]"
-    nest/$test_name
-    echo "$test_name [RUN] [OK]"
+
+    printf "\n===========================\n"
+    echo "TESTS ($optimization_mode)"
+    printf "===========================\n\n"
+
+    compile $build_dir $exe_name $debug_flags $optimization_mode;
+
+    printf "\n===========================\n"
+    echo "COMPILER BUILD OK"
+    printf "===========================\n\n"
+
+    for test_name in "${test_names[@]}"
+    do
+        printf "\n===========================\n"
+        echo "$test_name..."
+        printf "===========================\n\n"
+        build/nest "tests/$test_name.nat"
+        printf "\n===========================\n"
+        echo "$test_name [COMPILATION] [OK]"
+        printf "===========================\n\n"
+        nest/$test_name
+        printf "\n===========================\n"
+        echo "$test_name [RUN] [OK]"
+        printf "===========================\n\n"
+    done
 done
 
