3654552d892d5b4ec77b55143952409518d26d87
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-07-27 13:29:22 +0200

    Implement add + sub and minimal interpreter

--- debug.sh
+++ debug.sh
@@ -15,7 +15,7 @@ exe_name="nest"
 exe_path=$build_dir/$exe_name
 debug_flags="-g"
 optimization_flags="-O0"
-bootstrap_args="$path"
+bootstrap_args="$path i"
 case "$OSTYPE" in
     darwin*)  static=0;; 
     linux*)   static=1;;
@@ -26,6 +26,6 @@ compile $build_dir $exe_name $debug_flags $optimization_flags $static
 
 case "$OSTYPE" in
     darwin*)  lldb -- $exe_path $bootstrap_args;; 
-    linux*)   gf2 -ex b entry_point -ex r --args $exe_path $bootstrap_args;;
+    linux*)   gf2 -ex "set auto-solib-add off" -ex "r" --args $exe_path $bootstrap_args;;
     *)        echo "unknown: $OSTYPE" ;;
 esac
