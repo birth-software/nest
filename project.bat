71e2e4b6957e5e2184b87657460053efd345e534
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-09-20 09:36:44 -0600

    Some Windows integration

---
+++ project.bat
@@ -0,0 +1,1 @@
+clang -o build/build bootstrap/build.c -g -march=native -std=gnu2x -Wall -Wextra -Wpedantic -Wno-nested-anon-types -Wno-keyword-macro -Wno-gnu-auto-type -Wno-auto-decl-extensions -Wno-gnu-empty-initializer -Wno-fixed-enum-extension -pedantic -fno-exceptions -fno-stack-protector
