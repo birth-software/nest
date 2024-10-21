5be477b464f08c677c2f0a4e5a9661239467a476
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-10-21 05:49:23 -0600

    Pact with the devil: LLVM

--- project.ps1
+++ project.ps1
@@ -1,25 +1,33 @@
 Set-StrictMode -Version Latest
 Set-PSDebug -Trace 2
 $previous_error_action_preference = $global:ErrorActionPreference
+$global:ErrorActionPreference = 'Stop'
 $myargs=$args
 $build_dir="build"
 $release_mode="Debug"
 $build_type_prefix="build_type="
+$cmake_prefix_path_prefix="-DCMAKE_PREFIX_PATH="
+$cmake_prefix_path=""
 
 & {
     try
     {
-        $global:ErrorActionPreference = 'Stop'
-
-        if ($($myargs.Length) -ne 0 -and $myargs[0].StartsWith($build_type_prefix))
+        foreach ($arg in $myargs)
         {
-            $release_mode = $myargs[0].Substring($build_type_prefix.Length)
+            if ($arg.StartsWith($build_type_prefix))
+            {
+                $release_mode = $arg.Substring($build_type_prefix.Length)
+            }
+            if ($arg.StartsWith($cmake_prefix_path_prefix))
+            {
+                $cmake_prefix_path = $arg.Substring($cmake_prefix_path_prefix.Length);
+            }
         }
 
         Write-Output "Build type: $release_mode"
 
         New-Item -Path $build_dir -ItemType Directory -Force
-        cmake . "-B$build_dir" -G Ninja "-DCMAKE_BUILD_TYPE=$release_mode" -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++"
+        cmake . "-B$build_dir" -G Ninja "-DCMAKE_BUILD_TYPE=$release_mode" -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" "-DCMAKE_PREFIX_PATH=$cmake_prefix_path"
         pushd $build_dir
         ninja
         popd
--- project.sh
+++ project.sh
@@ -7,7 +7,19 @@ fi
 echo "Build type: $release_mode"
 build_dir=build
 mkdir -p $build_dir
-cmake . -B$build_dir -G Ninja -DCMAKE_BUILD_TYPE="$release_mode" -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++"
+
+case "$OSTYPE" in
+    linux*) CLANG_PREFIX="/usr/bin" ;; 
+    darwin*) CLANG_PREFIX="/opt/homebrew/opt/llvm/bin" ;;
+    *)        exit 1 ;;
+esac
+
+case "$OSTYPE" in
+    linux*) cmake . -B$build_dir -G Ninja -DCMAKE_BUILD_TYPE="$release_mode" -DCMAKE_C_COMPILER="$CLANG_PREFIX/clang" -DCMAKE_CXX_COMPILER="$CLANG_PREFIX/clang++" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=mold" ;;
+    darwin*) cmake . -B$build_dir -G Ninja -DCMAKE_BUILD_TYPE="$release_mode" -DCMAKE_C_COMPILER="$CLANG_PREFIX/clang" -DCMAKE_CXX_COMPILER="$CLANG_PREFIX/clang++" -DCMAKE_PREFIX_PATH=$(brew --prefix llvm) ;;
+    *)        exit 1 ;;
+esac
+
 original_dir=$PWD
 cd $build_dir
 ninja
