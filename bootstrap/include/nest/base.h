a5a94a16dbe4e8ec3525897f1cc4f757be4169cf
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-10-21 18:43:05 -0600

    Reduce redundant code

---
+++ bootstrap/include/nest/base.h
@@ -0,0 +1,10 @@
+#include <std/base.h>
+
+typedef enum CompilerBackend : u8
+{
+    COMPILER_BACKEND_NEST = 'm',
+    // COMPILER_BACKEND_LLVM,
+    COMPILER_BACKEND_COUNT,
+} CompilerBackend;
+
+
---
+++ bootstrap/include/nest/pdb_image.h
@@ -0,0 +1,2 @@
+#include <std/base.h>
+extern u8 pdb_image[143360];
