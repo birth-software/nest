0a98edb8099dd563733a45142a16ea83443ed2ae
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-12-17 21:32:58 -0600

    Start working towards Metal

--- bootstrap/std/render.c
+++ bootstrap/std/render.c
@@ -4,6 +4,71 @@
 #include <std/image_loader.h>
 #include <std/virtual_buffer.h>
 
+#if __APPLE__
+
+STRUCT(Renderer)
+{
+};
+
+typedef struct MTLDevice MTLDevice;
+extern MTLDevice* MTLCreateSystemDefaultDevice();
+
+Renderer* renderer_initialize(Arena* arena)
+{
+    auto* device = MTLCreateSystemDefaultDevice();
+    trap();
+}
+
+UVec2 renderer_font_compute_string_rect(Renderer* renderer, RenderFontType type, String string)
+{
+}
+
+void renderer_queue_font_update(Renderer* renderer, RenderWindow* window, RenderFontType type, TextureAtlas atlas)
+{
+}
+
+TextureIndex renderer_texture_create(Renderer* renderer, TextureMemory texture_memory)
+{
+}
+
+void renderer_window_frame_begin(Renderer* renderer, RenderWindow* window)
+{
+}
+
+void renderer_window_frame_end(Renderer* renderer, RenderWindow* window)
+{
+}
+
+RenderWindow* renderer_window_initialize(Renderer* renderer, OSWindow window)
+{
+}
+
+void window_queue_rect_texture_update(RenderWindow* window, RectTextureSlot slot, TextureIndex texture_index)
+{
+}
+
+void window_texture_update_begin(RenderWindow* window, BBPipeline pipeline_index, u32 descriptor_count)
+{
+}
+
+void window_rect_texture_update_begin(RenderWindow* window)
+{
+}
+
+void window_rect_texture_update_end(Renderer* renderer, RenderWindow* window)
+{
+}
+
+void window_render_rect(RenderWindow* window, RectDraw draw)
+{
+}
+
+void window_render_text(Renderer* renderer, RenderWindow* window, String string, Color color, RenderFontType font_type, u32 x_offset, u32 y_offset)
+{
+}
+
+#else
+
 #include <volk.h>
 
 #define MAX_SWAPCHAIN_IMAGE_COUNT (16)
@@ -2360,3 +2425,6 @@ U32Vec2 renderer_font_compute_string_rect(Renderer* renderer, RenderFontType typ
     auto result = texture_atlas_compute_string_rect(string, texture_atlas);
     return result;
 }
+
+#endif
+
